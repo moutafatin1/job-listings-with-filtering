e6075dcb2551e33b20ca8f63cbdb295d4970b663
Oussama Moutafatin <moutafatin3@gmail.com>
2023-02-12 18:07:00 +0100

    style guide, assets, and example data

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spartan: ["League Spartan", "sans-serif"],
+      },
+      colors: {
+        primary: "hsl(180, 29%, 50%)",
+        neutral: {
+          background: "hsl(180, 52%, 96%)",
+          filterTablet: "hsl(180, 31%, 95%)",
+          darkGrayishCyan: "hsl(180, 8%, 52%)",
+          veryDarkGrayishCyan: "hsl(180, 14%, 20%)",
+        },
+      },
+      backgroundImage: {
+        mobileHeader: "url('/images/bg-header-mobile.svg')",
+        desktopHeader: "url('/images/bg-header-desktop.svg')",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
-}
+};
